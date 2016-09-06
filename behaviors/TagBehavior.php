0846ae3aafdbe1e715494c6aaed2dbd9c748d667
xutl <79109681@qq.com>
2016-09-06 17:40:15 +0800

    i

--- behaviors/TagBehavior.php
+++ behaviors/TagBehavior.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Leaps Framework [ WE CAN DO IT JUST THINK IT ]
- * @link http://www.tintsoft.com/
- * @copyright Copyright (c) 2015 TintSoft Technology Co. Ltd.
- * @license http://www.tintsoft.com/license/
+ * @link https://github.com/creocoder/yii2-taggable
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
 
 namespace yuncms\tag\behaviors;

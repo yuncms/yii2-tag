35856aa00142a9617838590f81137073ed7ebab4
xutl <79109681@qq.com>
2016-09-14 14:28:35 +0800

    update

---
+++ migrations/m160823_055902_create_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+use common\models\Tag;
+
+/**
+ * Handles the creation for table `tag`.
+ */
+class m160823_055902_create_tag_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%tag}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'description' => $this->text(),
+            'pinyin' => $this->string(),
+            'letter' => $this->string(1),
+            'frequency' => $this->integer()->notNull()->defaultValue(0),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%tag}}');
+    }
+}
--- widgets/views/popular_tag.php
+++ widgets/views/popular_tag.php
@@ -8,9 +8,9 @@ use yii\helpers\Html;
  */
 ?>
 <?= Html::beginTag('ul', ['class' => $class]); ?>
-    <?php
-    foreach ($tags as $tag) {
-        echo Html::tag('li', Html::a($tag->name, ['/tag/tag/view', 'tag' => $tag->name], ['rel' => 'tag']));
-    }
-    ?>
+<?php
+foreach ($tags as $tag) {
+    echo Html::tag('li', Html::a($tag->name, ['/tag/tag/view', 'tag' => $tag->name], ['rel' => 'tag', 'target' => '_blank']));
+}
+?>
 <?= Html::endTag('ul'); ?>

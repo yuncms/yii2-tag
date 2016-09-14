e8505fd983542487aa9618d02b565a796bf0841d
xutl <79109681@qq.com>
2016-09-14 14:42:55 +0800

    update

---
+++ controllers/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\tag\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Class DefaultController
+ * @package yuncms\tag
+ */
+class DefaultController extends Controller
+{
+
+}

c9590638bd61e39ed01fbbc9f03ac6232dc1ec89
xutl <79109681@qq.com>
2016-09-23 18:06:25 +0800

    优化正则

--- models/Tag.php
+++ models/Tag.php
@@ -23,7 +23,7 @@ use Overtrue\Pinyin\Pinyin;
 class Tag extends ActiveRecord
 {
     /** @var string Default name regexp */
-    public static $nameRegexp = '/^[\w._-\x{4e00}-\x{9fa5}\#\+]+$/u';
+    public static $nameRegexp = '/^[\x{4e00}-\x{9fa5}\w\+\.\-_#]+$/u';
     // for gbk
     //public static $nameRegexp = '/^[\w._-\x80-\xff\#\+]+$/';
 
